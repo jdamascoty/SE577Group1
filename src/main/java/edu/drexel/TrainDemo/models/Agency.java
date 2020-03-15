faf485c1bffbd30549680d7ec7d3f090952c233b
jessicadamascoty <jessicadamascoty@gmail.com>
2020-03-15 10:55:39 -0400

    Initial Commit: Added paypal integration

---
+++ src/main/java/edu/drexel/TrainDemo/models/Agency.java
@@ -0,0 +1,48 @@
+package edu.drexel.TrainDemo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "agency")
+public class Agency {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "external_url")
+	private String externalUrl;
+
+	protected Agency() {
+	}
+
+	public Agency(String name, String externalUrl) {
+		this.name = name;
+		this.externalUrl = externalUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getExternalUrl() {
+		return externalUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "Agency{" + "id=" + id + ", name='" + name + '\'' + ", externalUrl='" + externalUrl + '\'' + '}';
+	}
+}
